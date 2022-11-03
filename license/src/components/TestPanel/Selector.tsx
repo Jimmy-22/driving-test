a21d0c26b8fac054fd503ae790f4223ce5326b12
ferrytsui <ferrytsui@163.com>
2022-11-03 16:13:47 +0800

    加载出试题选项

--- license/src/components/TestPanel/Selector.tsx
+++ license/src/components/TestPanel/Selector.tsx
@@ -1,7 +1,70 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback, useState } from 'react'
+import MyButton from '../MyButton'
+import './styles/selector.scss'
 
-const Selector: FC = () => {
-  return <div></div>
+interface IProps {
+  id: string
+  item1: string
+  item2: string
+  item3: string
+  item4: string
+}
+
+enum SELECTEORS {
+  item1 = '1',
+  item2 = '2',
+  item3 = '3',
+  item4 = '4',
+}
+
+const Selector: FC<IProps> = ({ id, item1, item2, item3, item4 }) => {
+  const [currentAnswer, setCurrentAnswer] = useState<SELECTEORS>(SELECTEORS.item1)
+
+  const handleSelectorClick = (e: React.MouseEvent): void => {}
+
+  const goNext: () => void = useCallback(() => {}, [])
+
+  return (
+    <div className="selector-panel" onClick={handleSelectorClick}>
+      <div className="item">
+        <div
+          className={['item-btn', currentAnswer === SELECTEORS.item1 ? ' active' : ''].join('')}
+          data-answer={SELECTEORS.item1}
+        >
+          {item1 || '加载中...'}
+        </div>
+      </div>
+      <div className="item">
+        <div
+          className={['item-btn', currentAnswer === SELECTEORS.item2 ? ' active' : ''].join('')}
+          data-answer={SELECTEORS.item2}
+        >
+          {item2 || '加载中...'}
+        </div>
+      </div>
+      {item3 && item4 && item3.length && item4.length && (
+        <>
+          <div className="item">
+            <div
+              className={['item-btn', currentAnswer === SELECTEORS.item3 ? ' active' : ''].join('')}
+              data-answer={SELECTEORS.item3}
+            >
+              {item3 || '加载中...'}
+            </div>
+          </div>
+          <div className="item">
+            <div
+              className={['item-btn', currentAnswer === SELECTEORS.item4 ? ' active' : ''].join('')}
+              data-answer={SELECTEORS.item4}
+            >
+              {item4 || '加载中...'}
+            </div>
+          </div>
+        </>
+      )}
+      <MyButton title="下一题" handleClick={goNext} />
+    </div>
+  )
 }
 
 export default Selector
--- license/src/components/TestPanel/index.tsx
+++ license/src/components/TestPanel/index.tsx
@@ -1,16 +1,23 @@
 import React, { FC } from 'react'
 import Question from './Question'
+import Selector from './Selector'
 import './styles/index.scss'
 
 interface IProps {
   question: string
   url: string
+  id: string
+  item1: string
+  item2: string
+  item3: string
+  item4: string
 }
 
-const TestPanel: FC<IProps> = ({ question, url }) => {
+const TestPanel: FC<IProps> = ({ question, url, id, item1, item2, item3, item4 }) => {
   return (
     <div className="test-panel">
       <Question question={question || '加载中...'} url={url} />
+      <Selector id={id} item1={item1} item2={item2} item3={item3} item4={item4} />
     </div>
   )
 }
